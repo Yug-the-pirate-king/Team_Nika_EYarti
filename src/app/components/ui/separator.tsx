6b3c771341baa4582d894a3d3f14c0b1db01ae5a
Yug Shah <yugshah197@gmail.com>
2026-09-07 14:30:30 +0530

    ai-upgrade: improve separator.tsx

--- src/app/components/ui/separator.tsx
+++ src/app/components/ui/separator.tsx
@@ -5,24 +5,38 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
 import { cn } from "./utils";
 
-function Separator({
-  className,
-  orientation = "horizontal",
-  decorative = true,
-  ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
-  return (
-    <SeparatorPrimitive.Root
-      data-slot="separator-root"
-      decorative={decorative}
-      orientation={orientation}
-      className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
-        className,
-      )}
-      {...props}
-    />
-  );
-}
+/**
+ * A visual divider that separates content.
+ *
+ * Built on top of Radix UI's Separator primitive, it supports both
+ * horizontal and vertical orientations and is decorative by default.
+ *
+ * @see https://www.radix-ui.com/primitives/docs/components/separator
+ */
+const Separator = React.forwardRef<
+  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(
+  (
+    { className, orientation = "horizontal", decorative = true, ...props },
+    ref,
+  ) => {
+    return (
+      <SeparatorPrimitive.Root
+        ref={ref}
+        data-slot="separator-root"
+        decorative={decorative}
+        orientation={orientation}
+        className={cn(
+          "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+          className,
+        )}
+        {...props}
+      />
+    );
+  },
+);
+
+Separator.displayName = "Separator";
 
 export { Separator };
